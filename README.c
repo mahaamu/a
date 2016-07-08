c6cdf4056f9451f5f0c6377bb3cc516a19713812
mahaamu <mahalakshmi3901@gmail.com>
2016-07-08 14:28:18 +0530

    Update and rename README.md to README.c
    finish

---
+++ README.c
@@ -0,0 +1,15 @@
+# a
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+char ch;
+printf("enter the character");
+scanf("%c",&i);
+if(ch='a';ch='A';ch='e';ch='E';ch='i';ch='I';ch='o';ch='O';ch='u';ch='U';)
+{
+printf("character is vowel");
+else
+printf("character is consonant");
+}
+}
